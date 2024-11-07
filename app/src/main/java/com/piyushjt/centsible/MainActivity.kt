0a0573a48058c6d4670c183ff370b870d8634e20
PiyushJT <piyushthummar7@gmail.com>
2024-11-07 18:53:19 +0530

    added logo and today's day/date (header)

--- app/src/main/java/com/piyushjt/centsible/MainActivity.kt
+++ app/src/main/java/com/piyushjt/centsible/MainActivity.kt
@@ -4,14 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.piyushjt.centsible.ui.theme.CentsibleTheme
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +30,87 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CentsibleTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
+
+                    Surface(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding)
+                    ) {
+
+                        MainScreen()
+
+                    }
                 }
             }
         }
     }
 }
 
+
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainScreen(
+
+) {
+
+    DayDate()
+
 }
 
-@Preview(showBackground = true)
+// Custom font
+val readexPro = FontFamily(
+    Font(R.font.readex_pro, FontWeight.Medium)
+)
+
 @Composable
-fun GreetingPreview() {
+fun DayDate() {
+    
+    Column(
+        modifier = Modifier
+            .padding(start = 24.dp)
+    ) {
+
+        Text(
+            text = SimpleDateFormat("EEEE,", Locale.getDefault()).format(Date()),
+            color = colorResource(id = R.color.light_text),
+            fontSize = 14.sp,
+            fontFamily = readexPro
+        )
+
+        Text(
+            text = SimpleDateFormat("dd MMMM", Locale.getDefault()).format(Date()),
+            color = colorResource(id = R.color.main_text),
+            fontSize = 18.sp,
+            fontFamily = readexPro
+        )
+    }
+
+
+}
+
+
+@Preview(
+    showSystemUi = true
+)
+@Composable
+fun CentsiblePreview() {
     CentsibleTheme {
-        Greeting("Android")
+        Scaffold(
+            modifier = Modifier.fillMaxSize()
+        ) { innerPadding ->
+
+            Surface(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding)
+            ) {
+
+                MainScreen()
+
+            }
+        }
     }
 }
